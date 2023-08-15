1a162c8ee65fd4df5d33ed2e517241c48db55693
Ian Richards <cdeveloper@hotmail.co.uk>
2023-08-15 22:02:51 +0100

    OneBookCosts8Pounds

--- PotterKata2/PotterKataTests.cs
+++ PotterKata2/PotterKataTests.cs
@@ -1,24 +1,57 @@
+using System.Threading.Channels;
+
 namespace PotterKata2;
 
 public class PotterKataTests
 {
+    private static Cart _cart;
+    
+    public PotterKataTests()
+    {
+        _cart = new Cart();
+    }
+    
     [Fact]
     public void NoBooksCostZero()
     {
         var expected = 0;
 
-        var cart = new Cart();
+        AssertExpectedCosts(expected);
+    }
+    
+    [Fact]
+    public void OneBooksCost8Pounds()
+    {
+        var expected = 8.00;
 
-        var actual = cart.GetTotal();
+        _cart.AddBook(PotterBooks.First);
         
+        AssertExpectedCosts(expected);
+    }
+
+    private void AssertExpectedCosts(double expected)
+    {
+        var actual = _cart.GetTotal();
         Assert.Equal(expected, actual);
     }
 }
 
+public enum PotterBooks
+{
+    First
+}
+
 public class Cart
 {
-    public int GetTotal()
+    private List<PotterBooks> _books = new();
+    
+    public double GetTotal()
     {
-        return 0;
+        return (_books.Count() == 1) ? 8.00 : 0;
+    }
+
+    public void AddBook(PotterBooks book)
+    {
+        _books.Add(book);
     }
 }
